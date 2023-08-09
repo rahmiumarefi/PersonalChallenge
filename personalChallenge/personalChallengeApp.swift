8a6fed9a11ac3293c5d418160cc8b475fb096b4c
rahmiumarefi <129579986+rahmiumarefi@users.noreply.github.com>
2023-08-09 19:12:43 +0700

    Initial Commit

---
+++ personalChallenge/personalChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  personalChallengeApp.swift
+//  personalChallenge
+//
+//  Created by Rahmi Umarefi on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct personalChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
